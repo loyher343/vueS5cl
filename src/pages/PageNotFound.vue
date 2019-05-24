14123dcd6c3f7d90a6cd1e9ea6c111f1e54bcc93
loyher343 <loyhernandez59@gmail.com>
2019-05-24 00:20:52 -0500

    added not Found Page with a redirect to home

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-full">
+        <h1>Not Found</h1>
+        <p>Oops, we couldn't find what you are looking for. Why don't you
+            <router-link :to="{name: 'Home'}">go home instead?</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+h1 {
+    font-size: 100px;
+}
+p {
+    font-size: 50px;
+    font-weight: lighter;
+}
+div {
+    text-align: center;
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../pages/PageHome'
 import ThreadShow from '../pages/PageThreadShow'
+import NotFound from '../pages/PageNotFound'
 
 Vue.use(Router)
 
@@ -21,7 +22,8 @@ export default new Router({
     {
       path: '*',
       name: "NotFound",
-      redirect: {name: 'Home'} // or '/'
+      component: NotFound
+      // redirect: {name: 'Home'} or '/'
       
     }
   ],
